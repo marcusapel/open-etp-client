e9fba12fc128d7316044dbd531c0ea604f961a59
Vadzim Kulyba <vadzim_kulyba@epam.com>
2023-04-12 19:24:56 +0000

    feat: added to etp client gc integration

--- src/lib/providers/google/index.ts
+++ src/lib/providers/google/index.ts
@@ -1,12 +1,12 @@
 // ============================================================================
-// Copyright 2022, EPAM Systems
-// Copyright 2022, Microsoft
+// Copyright 2023 Google LLC
+// Copyright 2023 EPAM Systems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,14 +15,4 @@
 // limitations under the License.
 // ============================================================================
 
-import { CloudContainer } from "./Container";
-import { ILogger, AbstractLogger } from "../common/Logging";
-
-export class LoggerFactory extends CloudContainer {
-  public static resolve(
-    provider: string,
-    args: { [key: string]: any } = {}
-  ): ILogger {
-    return CloudContainer.resolve(provider, AbstractLogger, args) as ILogger;
-  }
-}
+export { Logger as AzureLogger } from "./Logger";
--- src/lib/providers/index.ts
+++ src/lib/providers/index.ts
@@ -1,6 +1,7 @@
 // ============================================================================
 // Copyright 2022, EPAM Systems
 // Copyright 2022, Microsoft
+// Copyright 2023 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,5 +16,7 @@
 // limitations under the License.
 // ============================================================================
 
-export { Config, ConfigFactory } from './Config';
-export * as azure from './azure';
+export { Config, ConfigFactory } from "./Config";
+
+export * as azure from "./azure";
+export * as google from "./google";
