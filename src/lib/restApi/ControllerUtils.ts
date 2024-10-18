a8f9881a2baf75a93eb5674623c89664386c70ea
Laurent Deny <laurent.deny@emerson.com>
2024-10-17 20:56:54 -0500

    fix: add logging

--- src/lib/restApi/ControllerUtils.ts
+++ src/lib/restApi/ControllerUtils.ts
@@ -64,7 +64,10 @@ import { ResourceGraph } from "../common/ResponseHandlers";
 import { ErrorCode, EtpError } from "../common/EtpTypes";
 import { ApiProperty, ApiQueryOptions } from "@nestjs/swagger";
 import { IsUUID, Matches, MaxLength } from "class-validator";
-import e from "express";
+
+import logging from "../common/Logging";
+const logger = logging.getLogger("EtpClient");
+
 export { restApiMainUrl, restApiPort, restApiRoutePath };
 
 export const swaggerUIUrl = `${restApiMainUrl}:${restApiPort}${restApiRoutePath}`;
@@ -854,6 +857,9 @@ export const httpErrorFromEtpError = (error: unknown): HttpException => {
     ) {
       return new NotImplementedException({ description: error.message });
     }
+  }
+  if (error && typeof error === "object" && "message" in error) {
+    logger.error(error.message);
   }
   return new InternalServerErrorException({
     description: `Unknown Error: ${
